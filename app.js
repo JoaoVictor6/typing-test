7bd696c91d5355c8036fe587a687f4eea9ab1751
Joao Victor <santosjoao.dev@gmail.com>
2021-09-13 15:28:10 -0300

    add keyboard event

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const word = [
-  "teste"
+  "teste",
+  "soja",
+  "labirinto"
 ]
+const outputText = document.querySelector('.output__text')
+
+const typing = new TypingTest(word, outputText)
+
+typing.generateTextOutput(outputText)
 
 document.addEventListener('keypress', event => {
-  console.log(event.key)
+  const key = event.key
+  typing.listen(key)
 })
